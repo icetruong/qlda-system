cdd20684cab67cc1cb0a5ddc06d9f0d5457ded3e
icetruong <hocthidauthoi@gmail.com>
2026-05-02 17:03:48 +0700

    add auth-service

--- auth-service/src/main/java/com/qlda/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/qlda/authservice/AuthServiceApplication.java
@@ -1,9 +1,12 @@
 package com.qlda.authservice;
 
+import com.qlda.authservice.config.AuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AuthProperties.class)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
